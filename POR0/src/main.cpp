da8a4399234557098772267311edfa3151249e0d
Maciej Opaliński <poprostumieciek@gmail.com>
2021-04-21 13:47:14 +0200

    Use globals in POR0 solution (60/100)

--- POR0/src/main.cpp
+++ POR0/src/main.cpp
@@ -33,6 +33,12 @@ class Select
         {
             length = end - start + 1;
         }
+
+        void fixIndexing()
+        {
+            start--; end--;
+            setLength();
+        }
 };
 
 const int BASE = 29;
@@ -42,48 +48,51 @@ const int MAX_LEN = 1e6 + 2;
 long long base_powers[MAX_LEN];
 long long hash_table[MAX_LEN];
 
-void build_base_powers(int base = BASE, int mod = MOD)
+string word;
+Select first, second;
+
+void build_base_powers()
 {
     base_powers[0] = 1;
-    base_powers[1] = base;
+    base_powers[1] = BASE;
 
     size_t length = sizeof(base_powers) / sizeof(long long);
     for (size_t i = 2; i < length; i++)
     {
-        base_powers[i] = (base_powers[i-1] * base) % mod;
+        base_powers[i] = (base_powers[i-1] * BASE) % MOD;
     }
 }
 
-void build_hash_table(string word, int mod = MOD)
+void build_hash_table()
 {
     hash_table[0] = word[0] - 'a' + 1;
     for (size_t i = 1; i < word.size(); i++)
     {
         char c = word[i] - 'a' + 1;
 
-        hash_table[i] = ( hash_table[i-1] + (c * base_powers[i]) ) % mod;
+        hash_table[i] = ( hash_table[i-1] + (c * base_powers[i]) ) % MOD;
     }
 }
 
-bool hash_is_equal(string word, Select& f, Select& s, int mod = MOD)
+bool hash_is_equal(Select& f, Select& s)
 {
     // different sizes => substrings not equal
     if (f.length != s.length) return false;
     
     long long hash_f = hash_table[f.end] - hash_table[f.start - 1];
     long long hash_s = hash_table[s.end] - hash_table[s.start - 1];
-    if (hash_f < 0) hash_f += mod;
-    if (hash_s < 0) hash_s += mod;
+    if (hash_f < 0) hash_f += MOD;
+    if (hash_s < 0) hash_s += MOD;
 
     int power_index = s.start - f.start;
     long long multiply_by = base_powers[power_index];
-    hash_f = (hash_f * multiply_by) % mod;
+    hash_f = (hash_f * multiply_by) % MOD;
 
     // if hashes equal => substrings equal
     return hash_f == hash_s;
 }
 
-pair<int, int> bin_search_different_index(string word, Select& first, Select& second, int mod = MOD)
+pair<int, int> bin_search_different_index()
 {
     Select left_f, left_s;
     int left = 0;
@@ -103,7 +112,7 @@ pair<int, int> bin_search_different_index(string word, Select& first, Select& se
             left_s.setRange(left_s.start, left_s.start);
         }
 
-        if(hash_is_equal(word, left_f, left_s, mod))
+        if(hash_is_equal(left_f, left_s))
         {
             left = middle + 1;
         }
@@ -116,12 +125,12 @@ pair<int, int> bin_search_different_index(string word, Select& first, Select& se
     return make_pair(first.start + left, second.start + left);
 }
 
-Answer query(string word, Select& first, Select& second, int mod = MOD)
+Answer query()
 {
     // if they are equal => instant answer
-    if(hash_is_equal(word, first, second, mod)) return Answer::EQUAL;
+    if(hash_is_equal(first, second)) return Answer::EQUAL;
 
-    auto index = bin_search_different_index(word, first, second, mod);
+    auto index = bin_search_different_index();
     char f = word[index.first];
     char s = word[index.second];
 
@@ -134,23 +143,20 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    build_base_powers();
-
-    string word, output;
-    cin >> word;
-    build_hash_table(word);
-
     int queries;
+    cin >> word;
     cin >> queries;
 
-    Select first, second;
+    build_base_powers();
+    build_hash_table();
+
+    string output;
     int invert, answer;
     
     for (int i = 0; i < queries; i++)
     {
         cin >> first.start >> first.end >> second.start >> second.end;
-        first.start--; first.end--; second.start--; second.end--;
-        first.setLength(); second.setLength();
+        first.fixIndexing(); second.fixIndexing();
 
         invert = 1;
         if (first.start > second.start)
@@ -176,7 +182,7 @@ int main()
             }
         }
 
-        answer = query(word, first, second);
+        answer = query();
         if (answer == Answer::EQUAL)
         {
             if (first_trim)  answer = Answer::LARGER; // if first one was longer
