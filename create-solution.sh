1e2741380c1ed98db8570c911abde3eb5010c83f
Maciej Opaliński <poprostumieciek@gmail.com>
2021-06-30 09:00:11 +0200

    Add GTest and subsolutions to `create-solution.sh` script

--- create-solution.sh
+++ create-solution.sh
@@ -1,12 +1,17 @@
-name=$1
+root=$(pwd)/
+path=$1
+[ "${path: -1}" == "/" ] && path=${path%/}
+solution_name=$(echo $path | tac -s / | head -1)
 
-mkdir $name/
-cd $name/
+mkdir -p $path/
+cd $path/
 
 mkdir src/ test/
+# touch test/example.{in,out}
+cp $root/.templates/src.main.cpp.template ./src/main.cpp
+cp $root/.templates/test.main.cpp.template ./test/main.cpp
 
-touch test/example.{in,out}
-cp ../Makefile.template ./Makefile
-cp ../main.cpp.template ./src/main.cpp
+cp $root/.templates/Makefile.template ./Makefile
+sed -i "s/SOLUTION_NAME/${solution_name,,}/" ./Makefile
 
-sed -i "s/SOLUTION_NAME/${name,,}/" Makefile
+pwd
